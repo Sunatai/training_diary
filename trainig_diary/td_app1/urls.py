e5cd78d393193b1505944069c7a625ed7b6cbfeb
yuki yosigai <e175760@ie.u-ryukyu.ac.jp>
2020-07-29 16:37:49 +0900

    first

--- trainig_diary/td_app1/urls.py
+++ trainig_diary/td_app1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import DiaryList, DiaryDetail, DiaryCreate, DiaryDelete, DiaryUpdate, DiaryTest
+from .views import DiaryList, DiaryDetail, DiaryCreate, DiaryDelete, DiaryUpdate, DiaryTest, hello_template
+from . import views
 
 urlpatterns = [
     path('', DiaryList.as_view(), name='list'),
+    path('template/', views.hello_template, name='graph'),
     path('detail/<int:pk>', DiaryDetail.as_view(), name='detail'),
     path('create/', DiaryCreate.as_view(), name='create'),
     path('delete/<int:pk>', DiaryDelete.as_view(), name='delete'),
--- trainig_diary/training_daiary_project/settings.py
+++ trainig_diary/training_daiary_project/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'training_daiary_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'traning',
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASSWORD'),
+        'HOST':'',
+        'PORT':'',
     }
 }
+
 
 
 # Password validation
